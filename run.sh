743ff071a3d8d8ca34a3921a9922ec0b9a0b558c
Wesley Hales <wesleyhales@gmail.com>
2015-01-24 18:11:05 -0800

    new phantomjs 2.0 and command for extracting binary

--- run.sh
+++ run.sh
@@ -7,22 +7,17 @@ pushd ./data/postgres > /dev/null
     docker run -d -P -p 5432:5432 --name sg-postgres-name sg-postgres sh -c "./docker-entrypoint.sh postgres"
 popd > /dev/null
 #
-#pushd ./server > /dev/null
-#    docker rm -f sg-server-name
-#    docker build -t sg-server .
-#    docker run -d -P -p 80:8080 --name sg-server-name --link sg-postgres-name:spn sg-server sh -c "./server-entrypoint.sh"
-#popd > /dev/null
-
 pushd ./server > /dev/null
     docker rm -f sg-server-name
     docker build -t sg-server .
-    docker run -d -P -p 80:8080  -v /vagrant/bin:/root/phantomjs/bin --name sg-server-name --link sg-postgres-name:spn sg-server sh -c "./server-entrypoint.sh"
+    docker run -d -P -p 80:8080 --name sg-server-name --link sg-postgres-name:spn sg-server sh -c "./server-entrypoint.sh"
 popd > /dev/null
 
-#TIMEOUT=10m
-#echo "Running the container and triggering the tests (with ${TIMEOUT} timeout)..."
-#echo "Shared volume in ${SOURCE_PATH}. Current directory is ${pwd}."
-#timeout --signal=SIGKILL ${TIMEOUT} docker run -v $SOURCE_PATH:/source $CONTAINER sh -c "/source/container/build.sh"
-##docker run -ti -p 9923:9923 -v $SOURCE_PATH:/source $CONTAINER /bin/bash
-#echo "Finished with no error."
-#echo
+#the following is for fishing out the phantomjs binary built from source
+#pushd ./server > /dev/null
+#    docker rm -f sg-server-name
+#    docker build -t sg-server .
+#    docker run -d -P -p 80:8080  -v /vagrant/bin:/root/phantomjs2/ --link sg-postgres-name:spn sg-server
+##    docker exec -ti "container name" /bin/bash
+##cp -rf /root/phantomjs/bin/phantomjs /root/phantomjs2/
+#popd > /dev/null
