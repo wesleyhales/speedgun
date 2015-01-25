8c5a4adb929ccccb1b3fbcfc486ca86342ed3018
Wesley Hales <wesleyhales@gmail.com>
2015-01-25 07:33:42 -0800

    persist volumes for postgres data and server logs

--- run.sh
+++ run.sh
@@ -4,13 +4,13 @@ pushd ./data/postgres > /dev/null
     docker rm -f sg-postgres-name
     docker build -t sg-postgres .
     #docker run --net=host --rm -P --name sg-postgres-name sg-postgres
-    docker run -d -P -p 5432:5432 --name sg-postgres-name sg-postgres sh -c "./docker-entrypoint.sh postgres"
+    docker run -d -P -v /home/speedgun/postgres/data:/var/lib/postgresql/data -p 5432:5432 --name sg-postgres-name sg-postgres sh -c "./docker-entrypoint.sh postgres"
 popd > /dev/null
 #
 pushd ./server > /dev/null
     docker rm -f sg-server-name
     docker build -t sg-server .
-    docker run -d -P -p 80:8080 --name sg-server-name --link sg-postgres-name:spn sg-server sh -c "./server-entrypoint.sh"
+    docker run -d -P -v /home/speedgun/logs:/root/jboss-as-7.1.1.Final-fluxui/standalone/log -p 80:8080 --name sg-server-name --link sg-postgres-name:spn sg-server sh -c "./server-entrypoint.sh"
 popd > /dev/null
 
 #the following is for fishing out the phantomjs binary built from source
@@ -21,3 +21,6 @@ popd > /dev/null
 ##    docker exec -ti "container name" /bin/bash
 ##cp -rf /root/phantomjs/bin/phantomjs /root/phantomjs2/
 #popd > /dev/null
+
+#jump into running container
+#docker exec -ti sg-server-name /bin/bash
